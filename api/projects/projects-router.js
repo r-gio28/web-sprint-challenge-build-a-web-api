b4089ce2d447ecb30c6d043988e779ab470950f8
Giovanni  Rosales <giovannirosales@Giovannis-MacBook-Pro.local>
2021-12-02 23:21:06 -0600

    get endpoints are passing tests.

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,44 @@
 // Write your "projects" router here!
+const express = require('express')
+const Projects = require('./projects-model')
+const router = express.Router()
+
+router.use(express.json())
+
+router.get('/', (req, res) => {
+    Projects.get()
+    .then(projects => {
+        if (projects) {
+                res.status(200).json(projects)
+        } else {
+            res.status(200).json([])
+        }
+    })
+    .catch( error => {
+        console.log(error);
+        res.status(500).json({
+            message: "error ocurred while fetching projects"
+        })
+    })
+})
+
+router.get('/:id', (req, res) => {
+    Projects.get(req.params.id)
+    .then( project => {
+        if (project) {
+            res.status(200).json(project)
+        } else {
+            res.status(404).json({
+                message: "Project with requested id was not found"
+            })
+        }
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            message: "error ocurred while fetching projects"
+        })
+}) 
+})
+
+module.exports = router;
